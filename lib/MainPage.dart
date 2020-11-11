44decb0a8e228ed4d3b715a51f8d26d20606ad70
greylabsdev <greyson.dean@gmail.com>
2020-11-11 18:26:39 +0300

    Fixed background

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -12,75 +12,76 @@ class MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      constraints: BoxConstraints.expand(),
-      width: double.maxFinite,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: Image.network(
-        "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/web_bg.png",
-        fit: BoxFit.fitWidth,
-      ).image)),
-      child: Center(
-          child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 16.0),
-            CoverArtCard(
-              imageUrl:
-                  "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/cover_art.png",
-            ),
-            SizedBox(height: 16.0),
-            Text("UNUSUAL", style: Resources.textStyleTitle),
-            SizedBox(height: 6.0),
-            Text("by Threeactions", style: Resources.textStyleTitle),
-            SizedBox(height: 16.0),
-            MusicServiceCard(
-              title: "Yandex Music",
-              redirectUrl:
-                  "https://music.yandex.ru/album/12623767/track/72993620",
-              iconUrl:
-                  "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_yandex.png",
-            ),
-            SizedBox(height: 8.0),
-            MusicServiceCard(
-              title: "Apple Music",
-              redirectUrl:
-                  "https://music.apple.com/us/album/unusual/1538029037?i=1538029038",
-              iconUrl:
-                  "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_apple_music.png",
-            ),
-            SizedBox(height: 8.0),
-            MusicServiceCard(
-              title: "Spotify",
-              redirectUrl:
-                  "https://open.spotify.com/track/3NHfDEuRhrnpWN5hj5xRhr?si=NoxU-5qfSKebRbE7nXMALw",
-              iconUrl:
-                  "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_spotify.png",
-            ),
-            SizedBox(height: 8.0),
-            MusicServiceCard(
-              title: "VK Music",
-              redirectUrl:
-                  "https://vk.com/music/album/-2000248526_9248526_a1b169b1123ce6a99c",
-              iconUrl:
-                  "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_vk.png",
-            ),
-            SizedBox(height: 8.0),
-            MusicServiceCard(
-              title: "Youtube Music",
-              redirectUrl:
-                  "https://music.youtube.com/watch?v=RGL1Rf1_EXw&feature=share",
-              iconUrl:
-                  "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_youtube_music.png",
-            ),
-            SizedBox(height: 24.0),
-          ],
+        body: Stack(
+      children: [
+        Image.network(
+          "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/web_bg_cover.png",
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
         ),
-      )),
+        Center(
+            child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 16.0),
+              CoverArtCard(
+                imageUrl:
+                    "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/cover_art.png",
+              ),
+              SizedBox(height: 16.0),
+              Text("UNUSUAL", style: Resources.textStyleTitle),
+              SizedBox(height: 6.0),
+              Text("by Threeactions", style: Resources.textStyleTitle),
+              SizedBox(height: 16.0),
+              MusicServiceCard(
+                title: "Yandex Music",
+                redirectUrl:
+                    "https://music.yandex.ru/album/12623767/track/72993620",
+                iconUrl:
+                    "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_yandex.png",
+              ),
+              SizedBox(height: 8.0),
+              MusicServiceCard(
+                title: "Apple Music",
+                redirectUrl:
+                    "https://music.apple.com/us/album/unusual/1538029037?i=1538029038",
+                iconUrl:
+                    "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_apple_music.png",
+              ),
+              SizedBox(height: 8.0),
+              MusicServiceCard(
+                title: "Spotify",
+                redirectUrl:
+                    "https://open.spotify.com/track/3NHfDEuRhrnpWN5hj5xRhr?si=NoxU-5qfSKebRbE7nXMALw",
+                iconUrl:
+                    "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_spotify.png",
+              ),
+              SizedBox(height: 8.0),
+              MusicServiceCard(
+                title: "VK Music",
+                redirectUrl:
+                    "https://vk.com/music/album/-2000248526_9248526_a1b169b1123ce6a99c",
+                iconUrl:
+                    "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_vk.png",
+              ),
+              SizedBox(height: 8.0),
+              MusicServiceCard(
+                title: "Youtube Music",
+                redirectUrl:
+                    "https://music.youtube.com/watch?v=RGL1Rf1_EXw&feature=share",
+                iconUrl:
+                    "https://raw.githubusercontent.com/GreyLabsDev/ThreeactionsArea/main/img/icon_youtube_music.png",
+              ),
+              SizedBox(height: 24.0),
+            ],
+          ),
+        )),
+      ],
     ));
   }
 }
