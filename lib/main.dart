83f84895c082448ae1188ebc713f3f635368692c
sergey.shardyko <sergey.shardyko@skyeng.ru>
2024-01-16 01:02:34 +0300

    Enabled music release page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Threeactions',
-      home: MainPage(),
+      home: MusicReleasePage(),
     );
   }
 }
