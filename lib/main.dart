46a7e7f7ade889b70523670a941f14fad0d83958
GreyLabsDev <greyson.dean@gmail.com>
2024-01-18 19:57:22 +0300

    main page return

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:threeactions_area/pages/MusicReleasesPage.dart';
 import 'package:threeactions_area/pages/TestPage.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Threeactions',
-      home: TestPage(),
+      home: MusicReleasePage(),
     );
   }
 }
